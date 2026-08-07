c7bae26713fb0f5fa2976928dec1bc3a55cedb3b
FernandoPufe <57076663+agStatus@users.noreply.github.com>
2026-08-06 22:18:51 -0300

    Add files via upload

---
+++ lista.php
@@ -0,0 +1,14 @@
+<?php 
+include("conexao.php");
+
+$sql = "SELECT * FROM filmes";
+$resultado = $conn->query($sql);
+
+while($filme = $resultado->fetch_assoc()){
+	echo $filme['titulo'];
+}
+
+
+
+
+
