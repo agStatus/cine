2aa1ca4171f5f4b37d0899d285d147d833e22eda
FernandoPufe <57076663+agStatus@users.noreply.github.com>
2026-08-19 21:35:02 -0300

    Add files via upload

--- lista.php
+++ lista.php
@@ -3,10 +3,20 @@
 		<title>Lista de Filmes</title>		
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<link rel="stylesheet" href="style.css">
 	</head>
 	<body class="row">
 		<div class="col l12">
-			<div class="col l4 card-panel teal lighten-2">Esquerda</div>
+			<div class="col l4">
+				<ul>
+					<li><a href="#">Painel</a></li>
+					<li><a href="#">Filmes</a></li>
+					<li><a href="#">Categorias</a></li>
+				</ul>
+			</div>
+
+
+
 			<div class="col l8">Direita</div>
 		</div>
 	</body>
